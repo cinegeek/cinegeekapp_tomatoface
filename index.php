27ee714708e6b795f67e9f8e581a974724161bc5
cinegeek.net <yuhhhht@gmail.com>
2012-05-29 17:07:45 +0900

    maintain

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 $pattern = "/this$/i";
 $str = "this is this";
-echo preg_match($pattern, $str);  
+echo preg_match($pattern, $str); 
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	
+</body>
+</html>
--- index_commit.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pattern = "/this$/i";
-$str = "this is this";
-echo preg_match($pattern, $str);  
